71576c0b8b6ad129ab5365b5f612c95f7ce45294
AmtiX <107303548+AmtixDev@users.noreply.github.com>
2023-12-03 23:35:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,84 @@
 const morseCodeMap = {
-    A: ".-",
-    B: "-...",
-    C: "-.-.",
-    D: "-..",
-    E: ".",
-    F: "..-.",
-    G: "--.",
-    H: "....",
-    I: "..",
-    J: ".---",
-    K: "-.-",
-    L: ".-..",
-    M: "--",
-    N: "-.",
-    O: "---",
-    P: ".--.",
-    Q: "--.-",
-    R: ".-.",
-    S: "...",
-    T: "-",
-    U: "..-",
-    V: "...-",
-    W: ".--",
-    X: "-..-",
-    Y: "-.--",
-    Z: "--."
-  };
-  
-  function textToMorse(text) {
-    return text
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--.",
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  "_": "..--.-",
+  '"': ".-..-.",
+  $: "...-..-",
+  "@": ".--.-.",
+};
+
+function textToMorse(text) {
+  return text
       .toUpperCase()
       .split('')
       .map((char) => {
-        if (char === ' ') return ' ';
-        return morseCodeMap[char] || char;
+          if (char === ' ') return ' ';
+          return morseCodeMap[char] || char;
       })
       .join(' ');
-  }
-  
-  function morseToText(morse) {
-    const morseMap = Object.entries(morseCodeMap).reduce(
+}
+
+function morseToText(morse) {
+  const morseMap = Object.entries(morseCodeMap).reduce(
       (acc, [char, code]) => ({
-        ...acc,
-        [code]: char,
+          ...acc,
+          [code]: char,
       }),
       {}
-    );
-  
-    return morse
+  );
+
+  return morse
       .split(' ')
       .map((code) => morseMap[code] || code)
       .join('');
-  }
-  
+}
+
 module.exports = { textToMorse, morseToText };
